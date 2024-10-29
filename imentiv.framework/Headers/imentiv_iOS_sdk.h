d72f0fc04e2ff37d3e8bf8d5510e8e464b564837
shikilprakash333 <93506541+shikilprakash333@users.noreply.github.com>
2024-10-29 15:15:59 +0530

    Add files via upload

---
+++ imentiv.framework/Headers/imentiv_iOS_sdk.h
@@ -0,0 +1,18 @@
+//
+//  imentiv_iOS_sdk.h
+//  imentiv_iOS_sdk
+//
+//  Created by aster on 14/10/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for imentiv_iOS_sdk.
+FOUNDATION_EXPORT double imentiv_iOS_sdkVersionNumber;
+
+//! Project version string for imentiv_iOS_sdk.
+FOUNDATION_EXPORT const unsigned char imentiv_iOS_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <imentiv_iOS_sdk/PublicHeader.h>
+
+
